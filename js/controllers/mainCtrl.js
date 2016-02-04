83aeae21caa3dc83c7b2092e440b59b677ecd81e
Fernando Fernandez <ffmp777@yahoo.com>
2016-02-04 09:43:34 -0700

    resolved conflicts in Ctrl

--- js/controllers/mainCtrl.js
+++ js/controllers/mainCtrl.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 angular.module('myApp').controller('mainCtrl', ['$scope', 'itunesSvc', function ($scope, itunesSvc) {
     $scope.pods = [
         {
@@ -11,46 +10,18 @@ angular.module('myApp').controller('mainCtrl', ['$scope', 'itunesSvc', function 
             icon: 'http://www.wnyc.org/i/raw/1/Freakonomics_iTunes_Square-new.png',
             title: 'Freakonomics',
             intro: 'by: Stephen J. Dubner, Steven D. Levitt & New York Public Radio',
-=======
-angular.module('myApp').controller('mainCtrl', ['$scope', function ($scope) {
-    $scope.pods = [
-        {
-            icon: 'http://nationalpublicmedia.com/wp-content/uploads/2014/06/planetmoney.png',
-            by: 'npr',
-            title: 'Planet Money',
-            network: 'npr',
-            link: 'http: //www.npr.org/rss/rss.php?id=127413729'
-	  },
-        {
-            icon: 'http://www.wnyc.org/i/raw/1/Freakonomics_iTunes_Square-new.png',
-            by: 'Stephen J. Dubner & Steven D. Levitt',
-            title: 'Freakonomics',
-            network: 'New York Public Radio',
->>>>>>> 087df407bdb50c2ca301aa80e928a8dd5fe2f1dd
             // link: 'http://www.wnyc.org/articles/freakonomics-podcast/'
 	  },
         {
             icon: 'http://www.dancarlin.com/wp-content/uploads/2014/07/HH-current-239x239.jpg',
-<<<<<<< HEAD
             title: 'Hardcore History',
             intro: 'by: Dan Carlin',
-=======
-            by: 'Dan Carlin',
-            title: 'Hardcore History',
-            network: '',
->>>>>>> 087df407bdb50c2ca301aa80e928a8dd5fe2f1dd
             // link: 'https://itunes.apple.com/ca/podcast/dan-carlins-hardcore-history/id173001861?mt=2'
 	  },
         {
             icon: 'http://cdn.99percentinvisible.org/wp-content/uploads/powerpress/99invisible-logo-1400.jpg',
-<<<<<<< HEAD
             title: '99% Invisible',
             intro: 'by: Roman Mars for Radiotopia',
-=======
-            by: 'Roman Mars',
-            title: '99% Invisible',
-            network: 'Radiotopia',
->>>>>>> 087df407bdb50c2ca301aa80e928a8dd5fe2f1dd
             // link: 'http://feeds.99percentinvisible.org/99percentinvisible'
 	  }
 	];
--- js/services/itunesSvc.js
+++ js/services/itunesSvc.js
@@ -1,13 +1,47 @@
 angular.module('myApp').service('itunesSvc', function($http) {
 
-  this.getItunesData = function(searchTerm) {
+  this.getItunesData = function() {
     return $http.get("https://itunes.apple.com/us/rss/toppodcasts/limit=5/json").then(function(response){
 				console.log(response);
-        //return
 		}, function(error){
 				console.log(error);
 		})
   }
-
-  this.getItunesData('npr');
 })
+
+
+// var app = angular.module('itunes');
+//
+// app.service('itunesService', function($http, $q){
+//
+// var baseUrl = 'https://itunes.apple.com/search?term=';
+//     var _artist = '';
+//     var _finalUrl = '';
+//
+//     var makeUrl = function(artist) {
+//         _artist = artist;
+//         _artist = _artist.split(' ').join('+');
+//         _finalUrl = baseUrl + _artist + '&callback=JSON_CALLBACK';
+//         return _finalUrl;
+//     }
+//     this.setArtist = function(artist) {
+//         _artist = artist;
+//     }
+//     this.getArtist = function() {
+//         return _artist;
+//     }
+//     this.callItunes = function(artist) {
+//         //console.log('srvArtist', artist);
+//         makeUrl(artist);
+//         var deferred = $q.defer();
+//         $http({
+//             method: 'JSONP',
+//             url: _finalUrl
+//         }).success(function(data) {
+//             deferred.resolve(data.results);
+//         }).error(function(){
+//             deferred.reject('There was an error')
+//         })
+//         return deferred.promise;
+//     }
+// });
