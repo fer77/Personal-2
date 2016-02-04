ed5e77fba191b04b78232e366568073568ed6723
Fernando Fernandez <ffmp777@yahoo.com>
2016-02-04 09:56:55 -0700

    resolving conflicts

--- js/directives/appInfo.js
+++
@@ -1,9 +0,0 @@
-angular.module('myApp').directive('appInfo', function () {
-    return {
-        restrict: 'E',
-        scope: {
-            info: '='
-        },
-        templateUrl: 'js/directives/appInfo.html'
-    };
-});
