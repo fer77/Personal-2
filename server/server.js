c460da719b1e58a853148efcb806a4d05aa92213
Fernando Fernandez <ffmp777@yahoo.com>
2016-02-10 09:27:23 -0700

    nothing big. back-end stuff

--- server/server.js
+++ server/server.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
+var session = require('express-session')
 var cors = require('cors');
 
-var mainServerCtrl = require('../server/controllers/votesCtrl');
-
+var podcastsCtrl = require('./controllers/podcastsCtrl.js');
+var userCtrl = require('./controllers/userCtrl.js')
 var app = express();
 
 app.use(express.static(__dirname + './../js'));
@@ -15,9 +16,13 @@ var nodePort = 3000;
 var mongoUri = 'mongodb://localhost:27017/podcastApp-mongoose';
 
 // Endpoints
-app.post('/votes', votesCtrl.create);
-app.get('/votes', votesCtrl.read);
-app.put('/votes/:id', votesCtrl.update);
+app.post('/podcast', podcastsCtrl.create);
+app.get('/podcast', podcastsCtrl.read);
+app.put('/podcast/:id', podcastsCtrl.update);
+
+app.post('/user', userCtrl.create);
+app.get('/user', userCtrl.read);
+app.put('/user/:id', userCtrl.update);
 
 mongoose.set('debug', true);
 mongoose.connect(mongoUri);
